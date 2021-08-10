8ff0d267a9b72e47b809544b2bd4698e373e1a3d
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-08-10 11:50:02 +0800

    speaking works

--- wearapp/src/main/java/sg/edu/rp/c347/id19007966/wearapp/MainActivity.java
+++ wearapp/src/main/java/sg/edu/rp/c347/id19007966/wearapp/MainActivity.java
@@ -1,8 +1,14 @@
 package sg.edu.rp.c347.id19007966.wearapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import sg.edu.rp.c347.id19007966.wearapp.databinding.ActivityMainBinding;
 
@@ -10,6 +16,7 @@ public class MainActivity extends Activity {
 
     private TextView mTextView;
     private ActivityMainBinding binding;
+    private Button btnSpeak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,29 @@ public class MainActivity extends Activity {
         setContentView(binding.getRoot());
 
         mTextView = binding.text;
+
+        btnSpeak = findViewById(R.id.btnSpeak);
+
+        btnSpeak.setOnClickListener(view -> {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            startActivityForResult(intent, 1);
+        });
+
     }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode== RESULT_OK){
+            ArrayList<String> results = data.getStringArrayListExtra
+                    (RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+
+            Toast.makeText(MainActivity.this, spokenText,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
